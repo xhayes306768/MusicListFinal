db630ae79a3370156b2ea749ea2dea810dcf8c0a
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-12-08 15:23:56 -0600

    Built a DbContext class for the web app.

---
+++ MusicListFinal/Models/ArtistContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicListFinal.Models
+{
+    public class MusicContext:DbContext 
+    {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        { }
+        public DbSet<Music> Movies { get; set; } = null!;
+
+
+    }
+}
