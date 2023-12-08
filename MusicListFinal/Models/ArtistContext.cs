46a703b10f20d046b290f3b736f29bd676ef5dc7
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-12-08 15:48:37 -0600

    Seeded the table(s) with initial data.

--- MusicListFinal/Models/ArtistContext.cs
+++ MusicListFinal/Models/ArtistContext.cs
@@ -2,12 +2,37 @@
 
 namespace MusicListFinal.Models
 {
-    public class MusicContext:DbContext 
+    public class ArtistContext : DbContext
     {
-        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        public ArtistContext(DbContextOptions<ArtistContext> options) : base(options)
         { }
-        public DbSet<Music> Movies { get; set; } = null!;
 
+        public DbSet<Artist> Artists { get; set; } = null!;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //  initial data 
+            modelBuilder.Entity<Artist>().HasData(
+                new Artist
+                {
+                    ArtistId = 1,
+                    Name = "John Doe",
+                    DateOfBirth = new DateTime(1980, 5, 15),
+                    Songs = new List<string> { "Song1", "Song2", "Song3" },
+                    AwardsWon = 3
+                },
+                new Artist
+                {
+                    ArtistId = 2,
+                    Name = "Jane Smith",
+                    DateOfBirth = new DateTime(1995, 8, 20),
+                    Songs = new List<string> { "Song4", "Song5" },
+                    AwardsWon = 1
+                }
+               
+            );
+        }
     }
 }
